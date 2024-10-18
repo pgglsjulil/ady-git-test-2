3a3d8128d9fdca528c38056adb74925d9b505b79
Ady Ulil Amri <adyulilamri2@gmail.com>
2024-10-18 16:51:10 +0800

    add a paragraph and a list

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ab aut aspernatur illum, in autem porro, beatae quibusdam non atque consequatur sequi iure dolore impedit? Nemo similique voluptatibus doloribus maxime.</label></p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
